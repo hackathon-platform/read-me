e73ed840e4b854f34b2ea157b762f54c45c7be6c
Tobe Hinata <yangtaihubu@gmail.com>
2025-09-13 11:00:02 +0900

    headline-design-modified

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -68,7 +68,7 @@ export default async function Page() {
   return (
     <div className="animate-in fade-in duration-500 lg:mt-4 mt-2 w-full">
       {/* イベントのカルーセル */}
-      <div className="relative max-w-7xl mx-auto w-full pb-3">
+      <div className="relative max-w-7xl mx-auto w-full pb-3 pl-1">
         <h1 className="font-bold md:text-2xl">現在開催中のイベント</h1>
       </div>
       <EventsHeroCarousel
@@ -76,7 +76,7 @@ export default async function Page() {
       />
 
       {/* プロジェクト 3×2 */}
-      <div>
+      <div className="relative max-w-7xl mx-auto w-full pb-3 pl-1">
         <h1 className="font-bold py-3 md:text-2xl">
           プロジェクト
         </h1>
@@ -84,7 +84,7 @@ export default async function Page() {
       <section className="md:mx-auto max-w-7xl">
         <div className="-mx-3 flex flex-wrap">
           {projects.map((p) => (
-            <div key={p.id} className="w-full sm:w-1/2 lg:w-1/3 px-3 mb-6">
+            <div key={p.id} className=" px-3 mb-6 md:w-1/2 lg:w-1/3">
               <Link href={p.href} className="block h-48 mx-8 md:mx-auto md:h-auto">
                 <article className="h-full flex flex-col rounded-xl border bg-card shadow-sm hover:shadow-md transition">
                     {p.image?(
